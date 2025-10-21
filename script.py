d122c0b1ac0f60f354ab8bb5ce88cf246682fe71
aravindshenoy13 <aravindshenoywork@gmail.com>
2025-10-21 18:44:57 +0530

    Added dataset and noise type parser to auto-script

--- script.py
+++ script.py
@@ -1,22 +1,26 @@
 import subprocess
+import argparse
+
+parser = argparse.ArgumentParser('Pixel2Pixel')
+parser.add_argument('--dataset', default='kodak', type=str, help='Dataset name')
+parser.add_argument('--nt', default='gauss', type=str, help='Noise type')
+args = parser.parse_args()
 
 noise_levels = [10, 25, 50]
-noise_types = ["gauss", "poiss"]
 
 for nl in noise_levels:
-    for nt in noise_types:
-        cmd = [
-            "python","train.py",
-            "--num_iterations","1",
-            "--nn_layers","6",
-            "--epochs_per_iter","3000",
-            "--gt_dir","GT",
-            "--noisy_dir","Noisy",
-            "--nt",nt,
-            "--dataset","kodak",
-            "--loss","L1",
-            "--use_quality_weights","True",
-            "--nl",str(nl)
-        ]
-        print("Running:", " ".join(cmd))
-        subprocess.run(cmd)
+    cmd = [
+        "python","train.py",
+        "--num_iterations","1",
+        "--nn_layers","6",
+        "--epochs_per_iter","3000",
+        "--gt_dir","GT",
+        "--noisy_dir","Noisy",
+        "--nt",args.nt,
+        "--dataset",args.dataset,
+        "--loss","L1",
+        "--use_quality_weights","True",
+        "--nl",str(nl)
+    ]
+    print("Running:", " ".join(cmd))
+    subprocess.run(cmd)
